b33e2ca2bdc9c05a2df12d66a1e0a8b91b9af1ad
pseudocodenerd <shekharmadhav03@gmail.com>
2020-11-16 17:37:40 +0530

    init loginloop, things are looking good concept wise
    bhai please deadline extend karvao nahi ho payega varna

--- app/__main__.py
+++ app/__main__.py
@@ -7,11 +7,7 @@ from pyfiglet import Figlet
 import user
 
 def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-u', '--user', help='Type of user: (1) Counselor/Teacher (2) Student')
-    # args = parser.parse_args()
     load_dotenv(verbose=True)
-
     try:
         import colorama
         colorama.init()
@@ -44,17 +40,34 @@ def main():
         if db:
             cursor = db.cursor()
             cursor.execute('use {};'.format(os.getenv('DATABASE_NAME')))
-            console.print('Connection [green][b]successfull[/b][/green][blink]...[/blink]\n\nSetting up tables')
+            console.print('Connection [green][b]successfull[/b][/green][blink]...[/blink]\n\n :gear: Initialising Tables')
             # init and check for tables: user, counselor, teacher, sessions ...
             user.student_create_table(cursor)
-
-            
+            # admin.admin_create_table(cursor)
+            console.print('🔐 Login as ? \n (1) Counselor / Teacher \n (2) Student')
+            login = False
+            while not login:
+                inp = str(input('Please enter number to login as: '))
+                if inp == '1':
+                    console.print('Enter the [b]Admin Credentials [/b]')
+                elif inp == '2':
+                    console.print('Enter your admission number and password to login...')
+                    # user.student_create_prompt()
+                    admmno = str(input(':school: Enter your admission number: '))
+                    console.print('🔎 Searching for existing record in the database...')
+                    if user.exists(admmno):
+                        # ask for password, unhash and confirm login = True
+                        pass
+                    else:
+                        # create new user, ask for password, ask for details then show table to confirm reg and login = True
+                        pass
+                else:
+                    console.print('⚠️  Something went wrong... Please try again.')
     except:
-        console.print('There was some error connecting to the database.\n\n[bold red]Please make sure the database exists.[/bold red]')
+        console.print('There was some error connecting to the database.\n\n[bold red]⚠️  Something went wrong... Please try again.[/bold red]')
         exit
 
-    # login inquirer
-
+    # prompt starts now
 
 if __name__ == '__main__':
     main()
--- app/user.py
+++ app/user.py
@@ -1,19 +1,22 @@
 # user.py: user logging, creation, edits, delete
 
-import os
-
+import os, rich
 
 def student_create_table(cursor):
-    query = "create table students(AdmnNO char(6) NOT NULL, Name varchar(255) NOT NULL, Class char(2) NOT NULL, Stream varchar(4) NOT NULL, PRIMARY KEY (AdmnNO));"
+    console = rich.console.Console()
+    query = "create table if not exists students(AdmnNO char(6) NOT NULL, Name varchar(255) NOT NULL, Class char(2) NOT NULL, Stream varchar(4) NOT NULL, PRIMARY KEY (AdmnNO));"
+    this_dir, this_filename = os.path.split(__file__)
+    LOG_PATH = os.path.join(this_dir, "logs", "logs.txt")
+    to_print = 'bruh'
     try:
         cursor.execute(query)
-        to_print = '[CREATE] TABLE STUDENT: COMMAND'
-        this_dir, this_filename = os.path.split(__file__)
-        DATA_PATH = os.path.join(this_dir, "logs", "logs.txt")
-        with open(DATA_PATH, 'a') as log_file:
-            log_file.write(to_print)
-    except Exception as e:
-        if hasattr(e, 'message'):
-            print('[DB ERROR]:', e.message)
-        else:
-            print('[DB ERROR]:', e)
+        to_print = '[CREATE] TABLE STUDENT'
+    except Exception:
+        console.print(':bulb: Existing student table found ')
+        to_print = '[DB ERROR]: EXISTING TABLE FOUND'
+
+    with open(LOG_PATH, 'a') as log_file:
+        log_file.write(to_print+'\n')
+
+def exists(admmno):
+    console = rich.console.Console()
